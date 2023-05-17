b62eaf57e8e3cc261e18a84c85025a6d5eda803f
sayfullaevoloviddin <sayfullaevoloviddin73@gmail.com>
2023-05-17 22:39:30 +0500

    js 7-dars yuklandi
    js 7-darsning kodlari

---
+++ js/main.js
@@ -0,0 +1,77 @@
+/*Matn uzunligini topish*/
+/*let matn = "Assalomu alekum shogirdlar";
+console.log(matn.length)*/
+
+/*indexOf(matn ichidan so'zning boshlanish indexini topish)*/
+/*let matn = "Sen gapir sen men esa sen bilan yozib boramiz";
+let birinchiSenIndex = matn.indexOf('sen');
+console.log(matn.indexOf('sen',birinchiSenIndex+1));*/
+/*let matn = 'Salom hammaga! salom berdim hammaga!';
+
+/!*console.log(matn.replace(/salom|hammaga/gi,'alik'));*!/
+let yangimatn = matn.replace(/salom|hammaga/gi, function (soz){
+    if (soz == "Salom" || soz == "salom"){
+        return 'alik';
+    }
+    else if (soz == "hammaga"){
+        return 'barchaga';
+    }
+});
+console.log(yangimatn);*/
+
+/*Pop & Push (Arrayga ma'lumot kiritish va chiqarib olish) */
+/*let list = [];
+list.push('Fireman');
+list.push('Freedom');
+list.push('Police');
+list.push('Olov jakuy');
+list.push('Samandar batman');
+list.push('Ozod');
+
+console.log(list);
+console.log(list.length);
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);*/
+
+/*Shift & Unshift (Arrayga ma'lumot kiritish va chiqarish)*/
+/*
+let list =[];
+list.unshift('Olov');
+list.unshift("Komol");
+list.unshift('Bekzod');
+list.unshift(14);
+console.log(list)
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());
+console.log(list);
+*/
+
+/*Sort (tartibga keltirish)*/
+/*let list = ['Komol','Saman','Olov','Ozod','Bekzod','Muhammadamin','Shaxzod'];
+
+console.log(list.sort().reverse());*/
+/*let sonlar = [6,89,5,7,44,29,123,11];
+
+let tartiblangansonlar = sonlar.sort(function (a,b,){
+    return a-b;
+})
+
+console.log(tartiblangansonlar.reverse());*/
+
+
+
+
+
+
+
+
+
+
+
